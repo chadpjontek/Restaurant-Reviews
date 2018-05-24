ac5590a5c3f2132a62c5774dd73135b2b1871a72
Chad Pjontek <chadpjontek@gmail.com>
2018-05-24 10:41:23 -0400

    style: Change var to ES6 and Udacity style standard

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -1,5 +1,5 @@
-let restaurant;
-var map;
+let restaurant,
+  map;
 
 /**
  * Initialize Google map, called from HTML.
@@ -149,7 +149,7 @@ createReviewHTML = (review) => {
 /**
  * Add restaurant name to the breadcrumb navigation menu
  */
-fillBreadcrumb = (restaurant=self.restaurant) => {
+fillBreadcrumb = (restaurant = self.restaurant) => {
   const breadcrumb = document.getElementById('breadcrumb');
   const li = document.createElement('li');
   li.innerHTML = restaurant.name;
